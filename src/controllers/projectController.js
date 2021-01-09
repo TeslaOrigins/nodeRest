6012f7688a232e8ea09a834e9be478ec5bc71f3f
TeslaOrigins <matheuslourenco200@gmail.com>
2021-01-09 13:33:55 -0300

    paginação e nodemon adicionados

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -12,14 +12,25 @@ router.use(authMiddleware);
 router.get('/', async (req, res) => {
     try {
         const userName = req.query.name;
+        //const { paginationS, paginationL } = {paginationS: req.query.start, paginationL: req.query.length}
+        const start = parseInt(req.query.start, 10);
+        const length = parseInt(req.query.length, 10);
         var user = "";
+
         if(!userName){
             user = await User.find(); 
+            
+            return res.send({user});
+        } else if(!req.query.start & !req.query.length){
+            user = await User.find({name: {$regex: '.*' + userName + '.*', $options: "i"}});
+
+            return res.send({user});
         } else {
             user = await User.find({name: {$regex: '.*' + userName + '.*', $options: "i"}});
-        }        
+            const resultUsers = user.slice(start, length);
 
-        return res.send({user});
+            return res.send({resultUsers});
+        }        
     } catch (err) {
         console.log(err)
         return res.status(400).send({error: 'Error loading users'});
