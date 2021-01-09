d0fcf88791ba8e2919a31f2d5260968e8863529f
TeslaOrigins <matheuslourenco200@gmail.com>
2021-01-09 12:12:20 -0300

    Filtro por usuario adicionado

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -11,24 +11,32 @@ router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
     try {
-        const users = await User.find();
-
-        return res.send({users});
-    } catch (err) {
-        return res.status(400).send({error: 'Error loading users'})
-    }
-});
-
-router.get('/:userName', async (req, res) => {
-    try {
-        const userName = req.params;
-        const user = await User.find({name: {$regex: '.*' + userName + '.*'}});
+        const userName = req.query.name;
+        var user = "";
+        if(!userName){
+            user = await User.find(); 
+        } else {
+            user = await User.find({name: {$regex: '.*' + userName + '.*'}});
+        }        
 
         return res.send({user});
     } catch (err) {
-        console.log(err);
-        return res.status(400).send({error: 'Error loading user'})
+        console.log(err)
+        return res.status(400).send({error: 'Error loading users'});
     }
 });
+
+/*router.get('/:userName', async (req, res) => {
+    try {
+        const {userName} = req.params;
+        const user = await User.find({name: {$regex: '.*' + userName + '.*'}});
+
+        console.dir(user);
+        return res.send({user});
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send({error: 'Error loading user'});
+    }
+});*/
 
 module.exports = app => app.use('/projects', router);
